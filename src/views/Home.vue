55b68d29499f332cbe45bf3eaaf138c5934e410b
Sophia <sophia852686@gmail.com>
2022-08-31 16:45:43 +1000

    add computed and watch functions in setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,17 +15,58 @@
     {{ reactiveVariable.age }}
   </p>
   <button @click="updateReactive">Update Reactive</button>
+
+  <h1>How to use computed proprety inside of setup</h1>
+  <input type="text" v-model="search" />
+  <h3>Searching fruit is: {{ search }}</h3>
+  <div v-for="fruit in filteredFruits" :key="fruit">{{ fruit }}</div>
+
+  <button @click="callStopWatchFun">Stop watch function</button>
 </template>
 
 <script>
 // @ is an alias to /src
 //import Home from '@/components/Home.vue';
-import { ref, reactive } from "vue";
+import { ref, reactive, computed, watch, watchEffect } from "vue";
 
 export default {
   name: "Home",
 
   setup() {
+    const fruits = ref([
+      "apples",
+      "oranges",
+      "bananas",
+      "blueberries",
+      "pears",
+    ]);
+
+    const search = ref("");
+    //filtered fruits use computed methods
+    const filteredFruits = computed(() => {
+      //need to get fruits.value from ref first then fileter
+      return fruits.value.filter((fruit) => fruit.includes(search.value));
+    });
+
+    //use watch depends on search value changes
+    const stopWatch = watch(search, () => {
+      console.log("watch function runs when the serach value");
+    });
+
+    //use watchEffect will run first and depends on search value changes
+    const stopWatchEffect = watchEffect(() => {
+      console.log(
+        "watchEffect function runs first time and runs again when the serach value",
+        search.value
+      );
+    });
+
+    //this function is to stop watch
+    const callStopWatchFun = () => {
+      stopWatch();
+      stopWatchEffect();
+    };
+
     //create a ref in set up function
     const pRef = ref(null);
 
@@ -72,6 +113,10 @@ export default {
       language,
       reactiveVariable,
       updateReactive,
+      search,
+      fruits,
+      filteredFruits,
+      callStopWatchFun,
     };
   },
 
